dedb9129ff47bda0486fa2c7f49fdfbe02ab71b7
feit <feit@uber.com>
2019-10-23 17:00:27 -0700

    refine code

--- change_scp.sh
+++ change_scp.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-
+:<<EOF
 for i in {2..40}
 do
 	perl local/contrib/change_dir_inscp.pl /home/feit/Tool/kaldi/egs/voxceleb/v2 /media/feit/Data/Archive/Home/Tool/kaldi/egs/voxceleb/v2 /media/feit/Data/Archive/Home/Tool/kaldi/egs/voxceleb/v2/data/voxceleb1_test/split40utt/$i/feats.scp /media/feit/Data/Archive/Home/Tool/kaldi/egs/voxceleb/v2/data/voxceleb1_test/split40utt/$i/vad.scp
 done
+EOF
+
+perl local/contrib/change_dir_inscp.pl /home/feit/Tool/kaldi/egs/voxceleb/v2 /media/feit/Data/Archive/Home/Tool/kaldi/egs/voxceleb/v2 /media/feit/Data/Archive/Home/Tool/kaldi/egs/voxceleb/v2/data/voxceleb1_test/feats.scp /media/feit/Data/Archive/Home/Tool/kaldi/egs/voxceleb/v2/data/voxceleb1_test/vad.scp
--- run.sh
+++ run.sh
@@ -23,9 +23,9 @@ workid=_ladder		# work ID
 train_cmd=run.pl
 run_root=/media/feit/Work/Work/SpeakerID/Kaldi_Voxceleb/exp${workid}
 nnet_dir=/media/feit/Work/Work/SpeakerID/Kaldi_Voxceleb/exp${workid}/xvector_tf_but_test
-stage=4
+stage=6
 stage_end=9
-train_stage=-1		# -1 means the system need initialize the network
+train_stage=17		# -1 means the system need initialize the network
 iter=
 
 . ./cmd.sh
